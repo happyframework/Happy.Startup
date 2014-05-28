bd68b7829427909a19519918b07f51547797ee7b
dgw <shijiucha@qq.com>
2014-05-28 22:28:53 +0800

    dgw：自动注册插件开发。

---
+++ Happy.Startup.Ioc/AutoRegisterComponentPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Happy.Ioc;
+
+namespace Happy.Startup.Ioc
+{
+    /// <summary>
+    /// 自动将组件注册到Ioc容器中。
+    /// </summary>
+    public sealed class AutoRegisterComponentPlugin : IStartupPlugin
+    {
+        private readonly IComponentRegistry _componentRegistry;
+
+        public AutoRegisterComponentPlugin(IComponentRegistry componentRegistry)
+        {
+            Check.MustNotNull(componentRegistry, "componentRegistry");
+
+            _componentRegistry = componentRegistry;
+        }
+
+        // <inheritdoc />
+        public void OnStarting(StartupContext context)
+        {
+            _componentRegistry.AutoRegister(context.Assemblies);
+        }
+    }
+}
---
+++ Happy.Startup/StartupContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace Happy.Startup
+{
+    /// <summary>
+    /// 启动上下文。
+    /// </summary>
+    public sealed class StartupContext
+    {
+        internal StartupContext(IEnumerable<Assembly> assemblies)
+        {
+            Check.MustNotNull(assemblies, "assemblies");
+
+            this.Assemblies = assemblies;
+        }
+
+        /// <summary>
+        /// 参与启动的所有程序集。
+        /// </summary>
+        public IEnumerable<Assembly> Assemblies { get; private set; }
+    }
+}
